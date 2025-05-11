9297d437aa59194ad6acc4a363ac6a5fbd4dd41f
Bibek <zhabibek4u@gmail.com>
2025-05-11 20:47:47 +0530

    added all codes

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 
 import { toast } from "sonner";
 
-const API_URL = "http://localhost:8080";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export type ParaphraseResponse = {
   paraphrased_text: string;
